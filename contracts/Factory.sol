750900ce25e9a710187aa7bf1c2e1910f19016cc
etheriannn <rianziwalker@gmail.com>
2025-04-23 20:20:13 +0700

    Implement Factory contract with fee and owner initialization, and add create function stub

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,4 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.27;
 
-contract Factory {}
+import {Token} from "./Token.sol";
+
+contract Factory {
+    uint256 public immutable fee;
+    address public owner;
+
+    constructor(uint256 _fee) {
+        fee = _fee;
+        owner = msg.sender;
+    }
+
+    function create() {
+        // Create a new token 
+        // Save the token for later use
+        // List the token for sale
+        // Tell people it's live
+    }
+}
