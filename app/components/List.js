8e9c7c01ad5729a988acddaee3ddb3ecd73c37d1
etheriannn <rianziwalker@gmail.com>
2025-04-26 22:47:32 +0700

    Refactor List component to improve button functionality and formatting

--- app/components/List.js
+++ app/components/List.js
@@ -1,10 +1,13 @@
-import { ethers } from "ethers"
+import { ethers } from "ethers";
 
 function List({ toggleCreate, fee, provider, factory }) {
-
   return (
     <div className="list">
+      <p>Create token</p>
 
+      <button onClick={toggleCreate} className="btn--fancy">
+        [ cancel ]
+      </button>
     </div>
   );
 }
