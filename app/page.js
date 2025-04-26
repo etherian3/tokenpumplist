17b9ee0114a53a559b0b9363898cc687199f0fba
etheriannn <rianziwalker@gmail.com>
2025-04-26 19:54:20 +0700

    Fix formatting and improve consistency in page.js

--- app/page.js
+++ app/page.js
@@ -1,26 +1,27 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react"
-import { ethers } from 'ethers'
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Header from "./components/Header"
-import List from "./components/List"
-import Token from "./components/Token"
-import Trade from "./components/Trade"
+import Header from "./components/Header";
+import List from "./components/List";
+import Token from "./components/Token";
+import Trade from "./components/Trade";
+// import Footer from "./components/Footer";
 
 // ABIs & Config
-import Factory from "./abis/Factory.json"
-import config from "./config.json"
-import images from "./images.json"
+import Factory from "./abis/Factory.json";
+import config from "./config.json";
+import images from "./images.json";
 
 export default function Home() {
-
   return (
     <div className="page">
-
-      <h1 style={{ padding: "1em" }}>fun.pump</h1>
-
+      <Header
+        account={"0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"}
+        setAccount={""}
+      />
     </div>
   );
 }
