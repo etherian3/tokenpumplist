af79e6ee3130ad87dec8f9b049fb88fa010efece
etheriannn <rianziwalker@gmail.com>
2025-04-26 23:06:10 +0700

    Refactor List component to enhance token listing functionality and improve UI layout

--- app/components/List.js
+++ app/components/List.js
@@ -1,9 +1,25 @@
 import { ethers } from "ethers";
 
 function List({ toggleCreate, fee, provider, factory }) {
+  async function listHandler(form) {
+    const name = form.get("name");
+    const ticker = form.get("ticker");
+    console.log("Submited...", name, ticker);
+  }
+
   return (
     <div className="list">
-      <p>Create token</p>
+      <h2>List new token</h2>
+
+      <div className="list_description">
+        <p>Fee: {ethers.formatUnits(fee, 18)} ETH</p>
+      </div>
+
+      <form action={listHandler}>
+        <input type="text" name="name" placeholder="name" required />
+        <input type="text" name="ticker" placeholder="ticker" required />
+        <input type="submit" value="[ list ]" />
+      </form>
 
       <button onClick={toggleCreate} className="btn--fancy">
         [ cancel ]
