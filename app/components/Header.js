d59d9e75087d8a2871a8dff6c2d381dda45be980
etheriannn <rianziwalker@gmail.com>
2025-04-26 19:54:15 +0700

    Fix formatting and improve readability in Header component

--- app/components/Header.js
+++ app/components/Header.js
@@ -1,10 +1,12 @@
-import { ethers } from "ethers"
+import { ethers } from "ethers";
 
 function Header({ account, setAccount }) {
-
   return (
     <header>
-
+      <p className="brand">fun.pump</p>
+      <button className="btn--fancy">
+        [{account.slice(0, 6) + "..." + account.slice(38, 42)}]
+      </button>
     </header>
   );
 }
